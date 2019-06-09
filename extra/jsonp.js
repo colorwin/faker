683cc19b57b58857f5dc6cafb9ca3da5773f0062
colorwin <b504008585@163.com>
2019-06-09 22:26:00 +0800

    fix jsonp

--- extra/jsonp.js
+++ extra/jsonp.js
@@ -1,10 +1,14 @@
-function jsonp (params, url) {
+function jsonp (params, url, callback) {
     const script = document.createElement('script')
-    script.src = url + params.callback
+    let src = url + callback
+    for (let key in params) {
+        src += `&${key}=${params[key]}`
+    }
+    script.src = src
     document.appendChild(script)
 }
 
 function callback (data) {
     console.log(`jsonp data is ${data}`)
 }
-jsonp({foo: 'bar', callback: 'callback'}, 'http:www.baidu.com?callback=')
+jsonp({foo: 'bar'}, 'http:www.baidu.com?callback=', 'callback')
