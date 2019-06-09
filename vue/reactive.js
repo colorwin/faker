82aef1960a19f3042f9673dc9ecad871aaa8f72b
colorwin <b504008585@163.com>
2019-06-09 22:26:31 +0800

    vue reactive

---
+++ vue/reactive.js
@@ -0,0 +1,73 @@
+import { V4MAPPED } from "dns";
+
+const data = {
+    name: 'ycs'
+}
+observe(data)
+
+function updateComponent(value) {
+    vm._update()
+    // document.querySelector('div').innerHTML = value
+}
+
+new Watcher(data, 'name', updateComponent)
+data.name = 'foo'
+
+function observe(obj) {
+    Object.keys(obj).map((v, k) => {
+        defineReactive(obj, k, v)
+    })
+}
+
+function defineReactive (obj, k, v) {
+    if (isObject(v)) {
+        observe(v)
+    }
+    const dep = new Dep()
+    Object.defineProperty(obj, k, {
+        enumerable: true,
+        configurable: true,
+        get() {
+            if (Dep.target) {
+                dep.addSub(Dep.target)
+            }
+            return v
+        },
+        set(nV) {
+            v = nV
+            dep.notify()
+        }
+    })
+}
+
+class Dep {
+    constructor() {
+        this.subs = []
+    }
+    addSub(target) {
+        this.subs.push(target)
+    }
+    notify() {
+        this.subs.forEach(sub => {
+            sub.update()
+        })
+    }
+}
+Dep.target = null
+
+class Watcher {
+    constructor(data, key, cb) {
+        Dep.target = this
+        this.data = data
+        this.key = key
+        this.cb = cb
+        Dep.target = null
+    }
+    update() {
+        this.cb(this.data[this.key])
+    }
+
+    before () {
+        callHook('beforeUpdate')
+    }
+}
